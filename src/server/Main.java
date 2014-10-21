811337b9eb4d9a8a7d69be0aee402fa4de5d5244
andrius4669 <andrius4669@gmail.com>
2014-10-21 22:57:34 +0300

    server main class placeholder

---
+++ src/server/Main.java
@@ -0,0 +1,11 @@
+/** @author Andrius Lukas Narbutas */
+package server;
+
+public class Main {
+	private void cmain(String args[]) {
+		
+	}
+	public static void main(String args[]) {
+		(new Main()).cmain(args);
+	}
+}
