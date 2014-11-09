62f7f8e167f559d9121fc015a72a944885b0d531
dom4szz <dominykas.barzdys@gmail.com>
2014-11-09 16:14:09 +0200

    Create DeleteFileGUI.java

---
+++ src/client/DeleteFileGUI.java
@@ -0,0 +1,79 @@
+package client;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Domas
+ */
+public class DeleteFileGUI extends JFrame{
+    private JLabel title;
+    private JButton yes, no;
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private EditorGUI editor;
+    private int fileId;
+    public DeleteFileGUI(EditorGUI eGUI, int id){
+       super("Delete File");
+       editor = eGUI;
+       fileId = id;
+       setSize(300, 120);
+       setResizable(false);
+       setLayout(new GridBagLayout());
+        
+       Toolkit tk = Toolkit.getDefaultToolkit();
+       Dimension screenSize = tk.getScreenSize();
+       setLocation(screenSize.width/2-150, screenSize.height/2-50);
+        
+       Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
+       setIconImage(icon);
+       
+       title = new JLabel("Are you sure want to delete this file?");
+       yes = new JButton("Yes");
+       no = new JButton("No");
+       yes.setPreferredSize(new Dimension(100, 30));
+       no.setPreferredSize(new Dimension(100, 30));
+       gbc.insets = new Insets(5, 5, 5, 5);
+       gbc.gridwidth = 2;
+       add(title, gbc);
+       gbc.gridwidth = 1;
+       gbc.gridy = 1;
+       add(yes, gbc);
+       gbc.gridx = 1;
+       add(no, gbc);
+       setVisible(true);
+       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       yes.addActionListener(new onButtonClick());
+       no.addActionListener(new onButtonClick());
+       
+    }
+    private class onButtonClick implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource() == yes){
+                editor.deleteFile(fileId);
+                setVisible(false);
+                dispose();
+            }
+            if(e.getSource() == no){
+                setVisible(false);
+                dispose();
+            }
+        }
+
+        
+        
+    }
+    
+}
