ecd8ef2bcc557cde3d486e578141da5e52220bd8
dom4szz <dominykas.barzdys@gmail.com>
2014-11-09 20:03:03 +0200

    GUI for Sign in

---
+++ src/client/VerificationGUI.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2014, Domas
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package client;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Domas
+ */
+public class VerificationGUI extends JFrame{
+    JLabel title;
+    JTextField username;
+    JPasswordField password;
+    JButton submit, guestAccess;
+    GridBagConstraints gbc = new GridBagConstraints();
+    public VerificationGUI(){
+        super("Online text editor");
+        setSize(500, 500);
+        setResizable(false);
+        setLayout(new GridBagLayout());
+        
+        title = new JLabel("Administrator mode");
+        
+        username = new JTextField();
+        username.setPreferredSize(new Dimension(200, 22));
+        
+        password = new JPasswordField();
+        password.setPreferredSize(new Dimension(200, 22));
+        
+        submit = new JButton("Sign in");
+        submit.setPreferredSize(new Dimension(100, 22));
+        
+        guestAccess = new JButton("Guest access");
+        guestAccess.setPreferredSize(new Dimension(100, 22));
+        
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        gbc.gridwidth = 2;
+        add(title, gbc);
+        gbc.gridy++;
+        add(title, gbc);
+        gbc.gridy++;
+        add(username, gbc);
+        gbc.gridy++;
+        add(password, gbc);
+        gbc.gridy++;
+        gbc.gridwidth = 1;
+        add(guestAccess, gbc);
+        gbc.gridx = 1;
+        add(submit, gbc);
+        
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+    }
+}
