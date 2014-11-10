dacb08f1a87fa96adf5ab36cb34fc329a851f513
Domas <Domas@172.16.137.105>
2014-11-10 09:16:07 +0200

    Updated

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,16 +7,18 @@ import javax.swing.JFrame;
  * @author Domas
  */
 public class Client{
-
+    EditorGUI editor;
     public Client(){
         VerificationGUI verGUI = new VerificationGUI(this);
         verGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public void connect(){
-        
+        //TO-DO: connection with server
+        editor = new EditorGUI(this, EditorType.VIEW);
     }
     public void connect(String username, String password){
-        
+        //TO-DO: connection with server
+        editor = new EditorGUI(this, EditorType.EDIT);
     }
     public static void main(String[] args) {
        // ConnectingGUI cGUI = new ConnectingGUI();
